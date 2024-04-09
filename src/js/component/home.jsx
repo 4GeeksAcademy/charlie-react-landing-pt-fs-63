4e6e1d360775f1621d4e8da0d24a34e8106caff9
Charlie Willoughby <104572309+CWilloughby98@users.noreply.github.com>
2024-04-09 19:15:05 +0200

    Update home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Jumbotron from "./jumbotron";
 import Navbar from "./navbar"
 import Card from "./card"
-import cardData from "../cardData"
+import { cardData } from "../cardData"
 import Footer from "./footer"
 
 //create your first component
